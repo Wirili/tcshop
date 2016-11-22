c87a5d2f88e2c8b2d43c82444adde3628314470a
Wirili <332477694@qq.com>
2016-11-23 01:02:48 +0800

    Init

--- database/migrations/2016_11_21_211515_create_brands_table.php
+++ database/migrations/2016_11_21_211515_create_brands_table.php
@@ -16,7 +16,13 @@ class CreateBrandsTable extends Migration
         Schema::create('brand', function (Blueprint $table) {
             $table->increments('brand_id');
             $table->string('brand_name',60)->default('')->comment('品牌名称');
-            $table->string('brand_letter',60)->default('')->comment('品牌标记');
+            $table->string('brand_letter',60)->default('')->comment('品牌英文名称');
+            $table->string('brand_logo',255)->default('')->comment('品牌图片');
+            $table->text('brand_desc')->comment('品牌描述');
+            $table->string('site_url',255)->default('')->comment('品牌网站');
+            $table->unsignedSmallInteger('sort_order')->default(50)->comment('排序');
+            $table->boolean('is_show')->default(true)->comment('是否显示');
+            $table->boolean('audit_status')->default(true)->comment('是否审核');
             $table->timestamps();
         });
     }
